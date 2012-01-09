ba0bb0769be8c9ed924d12f8864927ba41617598
Vidur Murali <vidur.murali@gmail.com>
2012-01-09 12:53:52 +0530

    Refactored code a LOT. Added 'q' key to quit game. Created GameCharacter class.

--- Game.py
+++ Game.py
@@ -5,200 +5,212 @@ GAME_WIDTH = 1250
 GAME_HEIGHT = 750
 LEFT_KEY = pygame.K_LEFT
 RIGHT_KEY = pygame.K_RIGHT
+Q = pygame.K_q
 
-class Snoopy(object):
-	def __init__(self, game):
-		# Game to which this snoopy belongs to
-		self.game = game
-
-		# Get snoopy's image
-		self.image = pygame.image.load(os.path.join("assets/images", "snoopy.png"))
-
-		# Resize the huge snoopy
-		self.rect = self.image.get_rect()
-		height = self.rect.height
-		width = self.rect.width
-		self.image = pygame.transform.scale(self.image, (width/2, height/2))
-
-		# Reset the rect to the resized image's rect
-		self.rect = self.image.get_rect()
-
-		# Position him at the bottom of the game screen
-		self.rect.bottom = GAME_HEIGHT
-
-
-		self.speed = 10
-		self.bobbingAmplitude = 2
-		self.bobbingSpeed = self.bobbingAmplitude*2
-
-	def move(self, keys):
-		if keys and (self.isGoingLeft() and keys[RIGHT_KEY] or self.isGoingRight() and keys[LEFT_KEY]):
-			self.turnAround()
-
-		self.keepFlying()
-
-	def isGoingLeft(self):
-		return self.speed < 0
-
-	def isGoingRight(self):
-		return self.speed > 0
-
-	def keepFlying(self):
-		bob = random.randint(1,3)
-
-		newRect = self.rect.move([self.speed, self.bobbingSpeed])
-		if bob == 3:
-			newRect = self.rect.move([self.speed, self.bobbingSpeed])
-			self.bobbingSpeed = -self.bobbingSpeed
-		else:
-			newRect = self.rect.move([self.speed,0])
-
-		self.rect = newRect
-
-		if(self.isAtEdge()):
-			self.turnAround()
+class GameCharacter(object):
+    def __init(self):
+        self.hspeed = 0
+        self.vspeed = 0
+    
+    def isGoingLeft(self):
+        return self.hspeed < 0
+    
+    def isGoingRight(self):
+        return self.hspeed > 0
+    
+    def isAtEdge(self):
+        return (self.isAtLeftEdge() or self.isAtRightEdge())
+    
+    def isAtLeftEdge(self):
+        return (self.rect.left < 0)
+    
+    def isAtRightEdge(self):
+        return (self.rect.right > GAME_WIDTH)
+    
+    def isAtBottomEdge(self):
+        return (self.rect.bottom >= GAME_HEIGHT)
+    
+    def turnAround(self):
+        self.image = pygame.transform.flip(self.image, True, False)
+        self.hspeed = -self.hspeed
+    
+    def hasCollidedWith(self, otherObject):
+        return self.rect.colliderect(otherObject.rect)
+    
+    def setHSpeed(self, horizontalSpeed):
+        self.setSpeed(horizontalSpeed, None)
 
 
-	def isAtEdge(self):
-		return (self.isAtLeftEdge() or self.isAtRightEdge())
+    def setVSpeed(self, verticalSpeed):
+        self.setSpeed(None, verticalSpeed)
 
-	def isAtLeftEdge(self):
-		return (self.rect.left < 0)
 
-	def isAtRightEdge(self):
-		return (self.rect.right > GAME_WIDTH)
+    def setSpeed(self, horizontalSpeed, verticalSpeed):
+        if(horizontalSpeed):
+            self.hspeed = horizontalSpeed
 
-	def turnAround(self):
-		self.image = pygame.transform.flip(self.image, True, False)
-		self.speed = -self.speed
+        if(verticalSpeed):
+            self.vspeed = verticalSpeed
 
-class Woodstock(object):
-	def __init__(self, game, counter):
-		# Game to which this snoopy belongs to
-		self.game = game
-		
-		# The woodstock image
-		self.image = pygame.image.load(os.path.join("assets/images", "woodstock.png"))
-		
-		# Resize the woodstock
-		self.rect = self.image.get_rect()
-		height = self.rect.height
-		width = self.rect.width
-		self.image = pygame.transform.scale(self.image, (width/5, height/5))
+class Snoopy(GameCharacter):
+    def __init__(self, game):
+        # Game to which this snoopy belongs to
+        self.game = game
 
-		# Get the woodstock rect
-		self.rect = self.image.get_rect()
-		
-		# Get a random x axis location
-		width = self.rect.width
-		xcoordinate = random.randint(width/2, GAME_WIDTH - width/2)
-		
-		# Set the woodstock to the top of the game at the x location
-		self.rect.top = 0
-		self.rect.centerx = xcoordinate
-		
-		# Set the speed of the falling woodstock
-		# vspeed: vertical speed
-		
-		self.vspeed = counter*4
-		
-		# hspeed: horizontal speed
-		self.hspeed = random.randint(-15,15)
+        # Get snoopy's image
+        self.image = pygame.image.load(os.path.join("assets/images", "snoopy.png"))
 
-	def move(self):
-		
-		self.rect = self.rect.move([self.hspeed, self.vspeed])
+        # Resize the huge snoopy
+        self.rect = self.image.get_rect()
+        height = self.rect.height
+        width = self.rect.width
+        self.image = pygame.transform.scale(self.image, (width/2, height/2))
 
-		if(self.isAtEdge()):
-			self.turnAround()
+        # Reset the rect to the resized image's rect
+        self.rect = self.image.get_rect()
 
-	def isAtEdge(self):
-		return (self.isAtLeftEdge() or self.isAtRightEdge())
+        # Position him at the bottom of the game screen
+        self.rect.bottom = GAME_HEIGHT
 
-	def isAtLeftEdge(self):
-		return (self.rect.left < 0)
+        self.hspeed = 10
+        self.bobbingAmplitude = 2
+        self.bobbingSpeed = self.bobbingAmplitude*2
 
-	def isAtRightEdge(self):
-		return (self.rect.right > GAME_WIDTH)
+    def move(self, keys):
+        if keys and (self.isGoingLeft() and keys[RIGHT_KEY] or self.isGoingRight() and keys[LEFT_KEY]):
+            self.turnAround()
 
-	def turnAround(self):
-		self.image = pygame.transform.flip(self.image, True, False)
-		self.hspeed = -self.hspeed
+        self.keepFlying()
+
+    def keepFlying(self):
+        bob = random.randint(1,3)
+
+        newRect = self.rect.move([self.hspeed, self.bobbingSpeed])
+        if bob == 3:
+            newRect = self.rect.move([self.hspeed, self.bobbingSpeed])
+            self.bobbingSpeed = -self.bobbingSpeed
+        else:
+            newRect = self.rect.move([self.hspeed,0])
+
+        self.rect = newRect
+
+        if(self.isAtEdge()):
+            self.turnAround()
+
+class Woodstock(GameCharacter):
+    def __init__(self, game):
+        # Game to which this snoopy belongs to
+        self.game = game
+
+        # The woodstock image
+        self.image = pygame.image.load(os.path.join("assets/images", "woodstock.png"))
+
+        # Resize the woodstock
+        self.rect = self.image.get_rect()
+        height = self.rect.height
+        width = self.rect.width
+        self.image = pygame.transform.scale(self.image, (width/5, height/5))
+
+        # Get the woodstock rect
+        self.rect = self.image.get_rect()
+
+        # Get a random x axis location
+        width = self.rect.width
+        xcoordinate = random.randint(width/2, GAME_WIDTH - width/2)
+
+        # Set the woodstock to the top of the game at the x location
+        self.rect.top = 0
+        self.rect.centerx = xcoordinate
+
+        # Set default vertical speed (arbitrarily decided)
+        self.vspeed = 5
+
+        # hspeed: horizontal speed
+        self.hspeed = random.randint(-15,15)
+
+    
+    def move(self):
+        
+        self.rect = self.rect.move([self.hspeed, self.vspeed])
+        
+        if(self.isAtEdge()):
+            self.turnAround()
 
 class Game(object):
-	def run(self, width, height):
-		pygame.init()
-		self.screen = pygame.display.set_mode([width, height])
+    def __init__(self, width, height):
+        pygame.init()
+        self.screen = pygame.display.set_mode([width, height])
+        
+        # Game Title
+        pygame.display.set_caption("Catch the woodstocks!")
+        
+        # Initialize snoopy and the woodstocks
+        self.snoopy = Snoopy(self)
+        self.woodstocks = self.createWoodstocks()
 
-		# Game Title
-		pygame.display.set_caption("Catch the woodstocks!")
-		
-		# Initialize snoopy and the woodstocks
-		snoopy = Snoopy(self)
-		woodstocks = self.initWoodstocks()
+    def run(self):
+        # Define colors
+        black = (0,0,0)
 
-		# Define colors
-		black = (0,0,0)
-		
-		runGame = True
-		while runGame:
-			
-			self.woodstockCollide(woodstocks, snoopy)
-			#if (woodstock.rect.bottom >= GAME_HEIGHT) or woodstock.rect.colliderect(snoopy.rect):
-			#	woodstock = Woodstock(self)
+        runGame = True
+        while runGame:
 
-			keys = pygame.key.get_pressed()
+            self.resetDeadWoodstocks()
 
-			for event in pygame.event.get():
-				if event.type == QUIT:
-					runGame = False
-					break
+            keys = pygame.key.get_pressed()
 
-			# Move the woodstock
-			self.moveWoodstocks(woodstocks)
+            for event in pygame.event.get():
+                if event.type == QUIT or keys and keys[Q]:
+                    runGame = False
+                    break
 
-			snoopy.keepFlying()
-			snoopy.move(keys)
-			self.draw(snoopy, woodstocks)
-			pygame.display.update()
-	
-	def initWoodstocks(self):
-		# The 4 speeds are 4, 8, 12, 16
-		woodstocks = []
-		counter = 1
-		while counter <= 4:
-			woodstocks += [Woodstock(self, counter)]
-			counter += 1
-		
-		return woodstocks
-	
-	#	Checks if any of the woodstocks have collided with snoopy
-	def woodstockCollide(self, woodstocks, snoopy):
-		for i in xrange(len(woodstocks)):
-			if (woodstocks[i].rect.bottom >= GAME_HEIGHT) or woodstocks[i].rect.colliderect(snoopy.rect):
-				woodstocks[i] = Woodstock(self, (i+1))
-	
-	def moveWoodstocks(self, woodstocks):
-		for i in xrange(len(woodstocks)):
-			woodstocks[i].move()
-		
-		
-	def draw(self,snoopy, woodstocks):
-		self.drawSky()
-		self.drawWoodstocks(woodstocks)
-		self.screen.blit(snoopy.image, snoopy.rect)
-		
-		pygame.display.update()
-	
-	def drawSky(self):
-		skyimage = pygame.image.load(os.path.join("assets/images/sky", "sky1.jpg"))
-		skyimage = pygame.transform.scale(skyimage, (GAME_WIDTH, GAME_HEIGHT))
-		skyrect = skyimage.get_rect()
-		self.screen.blit(skyimage, skyrect)
-		
-	def drawWoodstocks(self, woodstocks):
-		for i in xrange(len(woodstocks)):
-			self.screen.blit(woodstocks[i].image, woodstocks[i].rect)
-		
-game = Game()
-game.run(GAME_WIDTH,GAME_HEIGHT)
+            self.moveWoodstocks()
+            self.snoopy.move(keys)
+            
+            self.draw()
+            pygame.display.update()
+
+    def createWoodstocks(self):
+        # There is one woodstock for each speed
+        speeds = [4, 8, 12, 16]
+        woodstocks = []
+        
+        for speed in speeds:
+            w = Woodstock(self)
+            w.setVSpeed(speed)
+            woodstocks += [w]
+
+        return woodstocks
+
+    #	Checks if any of the woodstocks have collided with snoopy
+    def resetDeadWoodstocks(self):
+        for w in self.woodstocks:
+            # Woodstock is dead if he hits the bottom or if Snoopy catches him
+            if w.isAtBottomEdge() or w.hasCollidedWith(self.snoopy):
+                vspeed = w.vspeed
+                self.woodstocks.remove(w)
+                w = Woodstock(self)
+                w.setVSpeed = vspeed
+                self.woodstocks.append(w)
+                
+
+    def moveWoodstocks(self):
+        for w in self.woodstocks:
+            w.move()
+
+    def draw(self):
+        self.drawSky()
+        for w in self.woodstocks:
+            self.screen.blit(w.image, w.rect)
+        self.screen.blit(self.snoopy.image, self.snoopy.rect)
+
+        pygame.display.update()
+
+    def drawSky(self):
+        skyimage = pygame.image.load(os.path.join("assets/images/sky", "sky1.jpg"))
+        skyimage = pygame.transform.scale(skyimage, (GAME_WIDTH, GAME_HEIGHT))
+        skyrect = skyimage.get_rect()
+        self.screen.blit(skyimage, skyrect)
+
+game = Game(GAME_WIDTH, GAME_HEIGHT)
+game.run()
